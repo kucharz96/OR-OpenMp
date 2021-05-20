fe423fcc133fa706759cc65330485795f9265d23
kucharz96 <kucharz96@wp.pl>
2021-05-20 14:21:03 +0200

    sekwencyjnie first

--- OR_projekt_2/sekwencyjnie.cpp
+++ OR_projekt_2/sekwencyjnie.cpp
@@ -1,20 +1,54 @@
-﻿// OR_projekt_2.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
-
-#include <iostream>
+﻿#include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int n = 100;
+    double** a = new double* [n];
+    for (int i = 0; i < n; ++i) {
+        a[i] = new double[n];
+    }
+
+    double** b = new double* [n];
+    for (int i = 0; i < n; ++i) {
+        b[i] = new double[n];
+    }
+
+    double** output = new double* [n];
+    for (int i = 0; i < n; ++i) {
+        output[i] = new double[n];
+    }
+
+
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j)
+        {
+            a[i][j] = 1.2;
+            b[i][j] = 2.5;
+            output[i][j] = 0;
+        }
+
+
+
+
+
+    // Multiplying matrix a and b and storing in array mult.
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j)
+            for (int k = 0; k < n; ++k)
+            {
+                output[i][j] += a[i][k] * b[k][j];
+            }
+
+    // Displaying the multiplication of two matrix.
+    cout << endl << "Output Matrix: " << endl;
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j)
+        {
+            cout << " " << output[i][j];
+            if (j == n - 1)
+                cout << endl;
+        }
+
+    return 0;
 }
-
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
